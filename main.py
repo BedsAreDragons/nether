b0af8f49df1d51dc971d7b669b3baa90b1015867
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-02-06 10:30:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Dummy data for passwords
-passwords = {}
-
-# Messages storage
 messages = []
 
 @app.route('/')
-def login():
-    return render_template('login.html')
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    return render_template('chat.html', messages=messages)
+def home():
+    return render_template('index.html', messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
-    password = request.form['password']
-    
-    if password == 'your_secret_password':
-        message = request.form['message']
-        messages.append({'username': 'Guest', 'message': message})
-    
-    return redirect(url_for('chat'))
+    username = request.form['username']
+    message = request.form['message']
+    messages.append({'username': username, 'message': message})
+    return jsonify({'status': 'success', 'message': message, 'username': username})
 
 if __name__ == '__main__':
     app.run(debug=True)
